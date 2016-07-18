491126272ce3a012dd28fae0bb65c75140e77a85
caritechsolutions <rrawlins@caritech.net>
2016-07-18 16:41:51 -0400

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 
 /* Add your OneAll Subdomain Here */
-var oneall_subdomain = 'http://thesunpass.com';
+var oneall_subdomain = 'thesunpass.com';
 
 /* Leave As Is */
 var oneall_connection_token = '';
